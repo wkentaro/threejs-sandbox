972b7dda9c5f7a1e372a7028ca476c6bd2ad9151
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-04-06 20:39:28 +0100

    Add more scenes

--- js/scene-0003.js
+++ js/scene-0003.js
@@ -8,9 +8,9 @@ var scene = new THREE.Scene();
 scene.background = new THREE.Color(0xeeeeee);
 
 resetCamera = function() {
-  camera.position.set(0, 0, 0);
+  camera.position.set(-0.1, 0, 1.4);
   camera.rotation.set(0, 0, 0);
-  camera.up.set(0, -1, 0);
+  camera.up.set(0, 0, 1);
 }
 
 camera = new THREE.PerspectiveCamera(30, 640 / 480, 0.01, 1000);
@@ -43,18 +43,48 @@ toggleRotateCamera = function() {
 
 $.ajax({
   type: 'GET',
-  url: '../examples/data/scene/0001-meta.json',
+  url: '../examples/data/scene/0003-meta.json',
   dataType: 'json',
   success: function(data) {
     for (var i = 0; i < data.poses.length; i++) {
       loadObject(data, index=i);
     }
-    controls.target.set(
-      data.poses[0].position[0], data.poses[0].position[1], data.poses[0].position[2]
-    );
+    var pose = data.poses[5];
+    controls.target.set(pose.position[0], pose.position[1], pose.position[2]);
+
+    for (var i = 0; i < data.camera_poses.length; i++) {
+      loadCameraMarker(data.camera_poses[i].position, data.camera_poses[i].quaternion);
+    }
   },
   async: false
 });
+
+function loadCameraMarker(position, quaternion) {
+  var points = [
+    new THREE.Vector3(0.013, 0.01, 0.03),
+    new THREE.Vector3(-0.013, 0.01, 0.03),
+    new THREE.Vector3(-0.013, -0.01, 0.03),
+    new THREE.Vector3(0.013, -0.01, 0.03),
+  ];
+  var material = new THREE.LineBasicMaterial({color: 0x0000ff});
+  for (var i = 0; i < points.length; i++) {
+    var geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), points[i]]);
+    var line = new THREE.Line(geometry, material);
+    line.position.set(position[0], position[1], position[2]);
+    line.quaternion.set(quaternion[0], quaternion[1], quaternion[2], quaternion[3]);
+    scene.add(line);
+
+    if (i < points.length - 1) {
+      var geometry = new THREE.BufferGeometry().setFromPoints([points[i], points[i + 1]]);
+    } else {
+      var geometry = new THREE.BufferGeometry().setFromPoints([points[i], points[0]]);
+    }
+    var line = new THREE.Line(geometry, material);
+    line.position.set(position[0], position[1], position[2]);
+    line.quaternion.set(quaternion[0], quaternion[1], quaternion[2], quaternion[3]);
+    scene.add(line);
+  }
+}
 
 function loadObject(data, index) {
   new THREE.MTLLoader(manager)
